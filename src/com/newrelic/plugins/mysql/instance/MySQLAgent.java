4cbb7f5a539421aaf9f0d2ef8376544463939e66
ronald.bradford@gmail.com <ronald.bradford@techcofounders.com>
2013-04-30 12:40:38 -0400

    Build Script and dependencies

--- src/com/newrelic/plugins/mysql/instance/MySQLAgent.java
+++ src/com/newrelic/plugins/mysql/instance/MySQLAgent.java
@@ -1,11 +1,6 @@
 package com.newrelic.plugins.mysql.instance;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
